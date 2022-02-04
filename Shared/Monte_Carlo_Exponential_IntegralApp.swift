adebc052616f793e17ff4071b8e7c8bd3a4c864f
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-04 15:54:07 -0600

    Initial Commit

---
+++ Shared/Monte_Carlo_Exponential_IntegralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monte_Carlo_Exponential_IntegralApp.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Monte_Carlo_Exponential_IntegralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
