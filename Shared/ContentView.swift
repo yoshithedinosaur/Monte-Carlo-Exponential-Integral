0de863ca1f84e558bc7cb12b6783d9615d869eae
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-10 14:24:46 -0600

    finished the computational aspect

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,16 +2,113 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by Yoshinobu Fujikake on 2/4/22.
+//  Created by Yoshi Fujikake on 2/10/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var finalVal = 0.0
+    @State var totalGuesses = 0.0
+    @State var totalIntegral = 0.0
+    @State var intervalStart = 0.0
+    @State var intervalEnd = 1.0
+    @State var guessString = "23458"
+    @State var totalGuessString = "0"
+    @State var integralString = "0.0"
+    
+    
+    // Setup the GUI to monitor the data from the Monte Carlo Integral Calculator
+    @ObservedObject var monteCarlo = IntegralCalculator(withData: true)
+    
+    //Setup the GUI View
     var body: some View {
-        Text("Hello, world!")
+        HStack{
+            
+            VStack{
+                
+                VStack(alignment: .center) {
+                    Text("Guesses")
+                        .font(.callout)
+                        .bold()
+                    TextField("# Guesses", text: $guessString)
+                        .padding()
+                }
+                .padding(.top, 5.0)
+                
+                VStack(alignment: .center) {
+                    Text("Total Guesses")
+                        .font(.callout)
+                        .bold()
+                    TextField("# Total Guesses", text: $totalGuessString)
+                        .padding()
+                }
+                
+                VStack(alignment: .center) {
+                    Text("Integral output")
+                        .font(.callout)
+                        .bold()
+                    TextField("# output", text: $integralString)
+                        .padding()
+                }
+                
+                Button("Cycle Calculation", action: {Task.init{await self.calculateIntegralVal()}})
+                    .padding()
+                    .disabled(monteCarlo.enableButton == false)
+                
+                Button("Clear", action: {self.clear()})
+                    .padding(.bottom, 5.0)
+                    .disabled(monteCarlo.enableButton == false)
+                
+                if (!monteCarlo.enableButton){
+                    
+                    ProgressView()
+                }
+                
+                
+            }
             .padding()
+            
+            Spacer()
+            
+        }
     }
+    
+    func calculateIntegralVal() async {
+        
+        
+        monteCarlo.setButtonEnable(state: false)
+        
+        monteCarlo.guesses = Int(guessString)!
+        monteCarlo.intervalStart = intervalStart
+        monteCarlo.intervalEnd = intervalEnd
+        monteCarlo.totalGuesses = Int(totalGuessString) ?? Int(0.0)
+        
+        await monteCarlo.calculateIntegralTotal()
+        
+        totalGuessString = monteCarlo.totalGuessesString
+        
+        integralString =  monteCarlo.integralString
+        
+        monteCarlo.setButtonEnable(state: true)
+        
+    }
+    
+    func clear(){
+        
+        guessString = "23458"
+        totalGuessString = "0.0"
+        integralString =  ""
+        monteCarlo.totalGuesses = 0
+        monteCarlo.totalIntegral = 0.0
+        monteCarlo.aboveData = []
+        monteCarlo.belowData = []
+        monteCarlo.firstTimeThroughLoop = true
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +116,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
--- Shared/IntegrateExponentialFunction.swift
+++ Shared/IntegrateExponentialFunction.swift
@@ -7,8 +7,71 @@
 
 import Foundation
 
-class IntegralCalculator: ObservableObject {
+class IntegralCalculator: NSObject, ObservableObject {
     
+    @MainActor @Published var aboveData = [(xPoint: Double, yPoint: Double)]()
+    @MainActor @Published var belowData = [(xPoint: Double, yPoint: Double)]()
+    @Published var totalGuessesString = ""
+    @Published var guessesString = ""
+    @Published var integralString = ""
+    @Published var enableButton = true
+    
+    var intervalStart = 0.0
+    var intervalEnd = 1.0
+    var finalVal = 0.0
+    var guesses = 1
+    var totalGuesses = 0
+    var totalIntegral = 0.0
+    var firstTimeThroughLoop = true
+    
+    @MainActor init(withData data: Bool){
+        
+        super.init()
+        
+        aboveData = []
+        belowData = []
+        
+    }
+    
+    /// calculate the value of the integral
+    ///
+    /// - Calculates the Value of exponential decay using Monte Carlo Integration
+    ///
+    /// - Parameter sender: Any
+    func calculateIntegralTotal() async {
+        
+        var maxGuesses: Int = 0
+        let boundingBoxCalculator = BoundingBox() ///Instantiates Class needed to calculate the area of the bounding box.
+        
+        let lengthOfSide1 = (intervalEnd-intervalStart)
+        let lengthOfSide2 = exp(-min(intervalStart,intervalEnd))
+        
+        
+        maxGuesses = guesses
+        
+        let newValue = await calculateMonteCarloIntegral(intervalStart: intervalStart, intervalEnd: intervalEnd, maxGuesses: maxGuesses)
+        
+        totalIntegral = totalIntegral + newValue
+        
+        totalGuesses = totalGuesses + guesses
+        
+        await updateTotalGuessesString(text: "\(totalGuesses)")
+        
+        //totalGuessesString = "\(totalGuesses)"
+        
+        ///Calculates the value of π from the area of a unit circle
+        
+        finalVal = totalIntegral/Double(totalGuesses) * boundingBoxCalculator.calculateSurfaceArea(numberOfDimensions: 2, lengthOfSide1: lengthOfSide1, lengthOfSide2: lengthOfSide2, lengthOfSide3: 0.0)
+        
+        await updateFinalValString(text: "\(finalVal)")
+        
+        //piString = "\(pi)"
+        
+       
+        
+    }
+    
+        
     /// calculateMonteCarloIntegral
     /// Parameter: intervalStart, intervalEnd, maxGuesses
     /// Return: itegralValMC
@@ -19,13 +82,131 @@ class IntegralCalculator: ObservableObject {
            _/  a
      */
     func calculateMonteCarloIntegral (intervalStart: Double, intervalEnd: Double, maxGuesses: Int) async -> Double {
-        let boundingBoxCalculator = BoundingBox() /// Instantiates class needed to calculate the integral
         
-        var integralValMC = 0.0
+        var numberOfGuesses: Int = 0
+        var point = (xPoint: 0.0, yPoint: 0.0)
+        var xRange: [Double] = [0.0, 0.0]
+        var yRange: [Double] = [0.0, 0.0]
+        var guessPoint = 0.0
+        var pointsBelowFunc: Int = 0
+        
+        xRange = [intervalStart, intervalEnd]
+        yRange = [0.0, exp(-min(intervalStart,intervalEnd))]
+        
+        var integralVal = 0.0
+        var newAbovePoints : [(xPoint: Double, yPoint: Double)] = []
+        var newBelowPoints : [(xPoint: Double, yPoint: Double)] = []
+        
+        while numberOfGuesses < maxGuesses {
+            point.xPoint = Double.random(in: xRange[0]...xRange[1])
+            point.yPoint = Double.random(in: yRange[0]...yRange[1])
+            
+            guessPoint = exp(-point.xPoint)
+            
+            if (point.yPoint <= guessPoint) {
+                pointsBelowFunc += 1
+                
+                newBelowPoints.append(point)
+            } else {
+                newAbovePoints.append(point)
+            }
+            
+            numberOfGuesses += 1
+        }
+        
+        //Appended from Dr. Terry's code
+        //Append the points to the arrays needed for the displays
+        //Don't attempt to draw more than 250,000 points to keep the display updating speed reasonable.
+        
+        if ((totalGuesses < 500001) || (firstTimeThroughLoop)){
+        
+//            insideData.append(contentsOf: newInsidePoints)
+//            outsideData.append(contentsOf: newOutsidePoints)
+            
+            var plotAbovePoints = newAbovePoints
+            var plotBelowPoints = newBelowPoints
+            
+            if (newAbovePoints.count > 750001) {
+                
+                plotAbovePoints.removeSubrange(750001..<newAbovePoints.count)
+            }
+            
+            if (newBelowPoints.count > 750001){
+                plotBelowPoints.removeSubrange(750001..<newBelowPoints.count)
+                
+            }
+            
+            await updateData(abovePoints: plotAbovePoints, belowPoints: plotBelowPoints)
+            firstTimeThroughLoop = false
+        }
+        
+        integralVal = Double(pointsBelowFunc)
+        
+        return integralVal
+    }
+    
+    
+    /// updateData
+    /// The function runs on the main thread so it can update the GUI
+    /// - Parameters:
+    ///   - insidePoints: points inside the circle of the given radius
+    ///   - outsidePoints: points outside the circle of the given radius
+    @MainActor func updateData(abovePoints: [(xPoint: Double, yPoint: Double)] , belowPoints: [(xPoint: Double, yPoint: Double)]){
+        
+        aboveData.append(contentsOf: abovePoints)
+        belowData.append(contentsOf: belowPoints)
+    }
+    
+    
+    /// updateTotalGuessesString
+    /// The function runs on the main thread so it can update the GUI
+    /// - Parameter text: contains the string containing the number of total guesses
+    @MainActor func updateTotalGuessesString(text:String){
+        
+        self.totalGuessesString = text
+        
+    }
+    
+    /// updateFinalValString
+    /// The function runs on the main thread so it can update the GUI
+    /// - Parameter text: contains the string containing the current value of the integral
+    @MainActor func updateFinalValString(text:String){
+        
+        self.integralString = text
+        
+    }
+    
+    /// setButton Enable
+    /// Toggles the state of the Enable Button on the Main Thread
+    /// - Parameter state: Boolean describing whether the button should be enabled.
+    @MainActor func setButtonEnable(state: Bool){
         
         
+        if state {
+            
+            Task.init {
+                await MainActor.run {
+                    
+                    
+                    self.enableButton = true
+                }
+            }
+            
+            
+                
+        }
+        else{
+            
+            Task.init {
+                await MainActor.run {
+                    
+                    
+                    self.enableButton = false
+                }
+            }
+                
+        }
         
-        return integralValMC
     }
     
 }
